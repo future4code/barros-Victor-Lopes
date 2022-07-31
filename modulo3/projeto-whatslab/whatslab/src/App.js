b9c6e1fa6cc7ac32e1af0438d65ae7043fa585c2
Victor Luiz Lopes <102486038+victor-lopes13@users.noreply.github.com>
2022-07-31 15:06:41 -0300

    projeto

---
+++ modulo3/projeto-whatslab/whatslab/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import WhatsLab from './components/WhatsLab';
+
+function App() {
+  return (
+    <div className="App">
+      <WhatsLab></WhatsLab>
+    </div>
+  );
+}
+
+export default App;
